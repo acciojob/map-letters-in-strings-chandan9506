c978e0624e5578c04334552db764ace3d53e4098
chandan9506 <152258839+chandan9506@users.noreply.github.com>
2024-04-12 11:43:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function mapLetters(str) {
+	let obj = {};
+	for (let i = 0; i < str.length; i++) {
+		let t = str[i];
+		if(obj[t] == undefined){
+			obj[t] = [i];
+		}
+		else{
+			obj[t].push(i);
+		}
+	}
+	return obj;
+}
+const str = prompt("enter a string: ");
+alert(JSON.stringify(mapLetters(str)));
